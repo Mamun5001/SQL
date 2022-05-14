91720f7e0c80fa6dd64b29dc1802234111ecfd26
MD AL MAMUN <78454580+Mamun5001@users.noreply.github.com>
2022-05-14 02:16:32 +0200

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE test;
+USE test;
+CREATE TABLE employee(
+EMPLOYEE_ID int,
+FIRST_NAME varchar (50),
+LAST_NAME varchar (50),
+EMAIL varchar (100),
+PHONE_NUMBER int,
+HIRE_DATE date,
+JOB_ID varchar (50),
+SALARY int);
+
+SELECT * from employee;
